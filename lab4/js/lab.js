9c9e8e3af1866dacc4187f23352b1902638d82ea
75pearsinabunch <75pearsinabunch@gmail.com>
2021-01-25 10:23:39 -0800

    Update lab.js

---
+++ lab4/js/lab.js
@@ -0,0 +1,9 @@
+// Task 2: computer task
+// How to print "Hello world" in Python
+// 1. Turn on a computer
+// 2. Open a web browser
+// 3. Search "Python Online IDE" in the search bar
+// 4. Select a website from the search request
+// 5. Type into the text box "print("Hello world")"
+// 6. Click run or go (depending the website)
+// 7. enjoy python
