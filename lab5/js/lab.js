1d988c0aae759340a68c3d4b88d915ea543e8574
75pearsinabunch <75pearsinabunch@gmail.com>
2021-01-27 18:24:07 -0800

    Update lab.js

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+* Author: Alvaro Perez <aleperez@ucsc.edu>
+* Created 1 Janurary
+* License: Public Domain
+*/
+
+// Define Variable
+var make ="Ford";
+var model = "Taurus";
+var color = "Rusty";
+var year = 1995;
+
+// calculate
+var age = 2021 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
