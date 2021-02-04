10b1e1b05cece485aa7440529d02f7d85d2b2115
75pearsinabunch <75pearsinabunch@gmail.com>
2021-02-03 17:36:10 -0800

    Update lab.js

--- Section/js/lab.js
+++ Section/js/lab.js
@@ -2,3 +2,26 @@ var varname = "SectionC";
 console.log(varname);
 varname = "I changed section";
 console.log(varname);
+//2-2-2021
+console.log("my Javascript loaded");
+var nameVar = "Toni";
+var numnNumb = 17;
+//console.log(nameVar);
+//console.log(numNumb);
+numNumb = 25;
+//console.log(numNumb);
+numNumb = 4;
+console.log(numNumb);
+function firstFunction(){
+  console.log("This is my first function");
+}
+firstFunction();
+
+function testBigger (firstnum, secondNum){
+  console.log(firstnum>secondNum);
+}
+
+var nameVar = "Toni";
+var numnNumb = 17;
+firstFunction();
+testBigger();
