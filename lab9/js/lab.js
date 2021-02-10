5476c5fe95ff24427bf7d8a747230ad4437f8cfe
75pearsinabunch <75pearsinabunch@gmail.com>
2021-02-10 11:39:01 -0800

    Update lab.js

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -10,5 +10,13 @@ var new1El = document.createElement("p");
 new1El.innerHTML = "I have transformed!!!";
 var new2El = document.createElement("h1");
 new2El.innerHTML = "So, what is different?";
+
+var new3El = document.createElement("h1");
+var newtext = document.createTextNode("This is suppose to be last.");
+new3El.appendChild(newtext);
+
 document.body.appendChild(new1El);
 document.body.appendChild(new2El);
+
+var newchild = document.getElementById("p1");
+outputEl.insertBefore(new3El,newchild);
