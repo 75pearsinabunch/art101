1ee4a70ea94525b3eb1259a16d975e39397f5a2d
75pearsinabunch <75pearsinabunch@gmail.com>
2021-02-10 08:50:08 -0800

    Create script2129.js

---
+++ Section/js/script2129.js
@@ -0,0 +1,5 @@
+console.log(document.getElementById("div_1").innerHTML);
+console.log(document.getElementById("div_1"));
+function funcName(){
+  console.log(document.getElementById("div_1").innerHTML="This is more annoying.");
+}
