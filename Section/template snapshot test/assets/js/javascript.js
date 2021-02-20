34bd9ac0755636a07a203c03470bdb2a280d3628
75pearsinbunch <75pearsinabunch@gmail.com>
2021-02-19 17:20:41 -0800

    Update javascript.js

--- Section/template snapshot test/assets/js/javascript.js
+++ Section/template snapshot test/assets/js/javascript.js
@@ -1,3 +1,4 @@
+//change name, email. and message to user's input 2/16/21
 function submitBio(){
 var nameVar = document.getElementById("name").value;
 console.log(nameVar);
@@ -9,3 +10,14 @@ document.getElementById("name_email_id").innerHTML = nameVar +" EMAIL: " + email
 document.getElementById("bio_id").innerHTML = messageVar;
 }
 document.getElementById("bio_submit_id").addEventListener('click',submitBio);
+
+// chnages name, email, and meassge color to green 2/18/21
+// function colorMe(){
+  //document.getElementById('name_email_id').style.color = "green";
+//   document.getElementById('name_email_id').className+=""+"color_me";
+// }
+// document.getElementById("color_me_id").addEventListener('click', colorMe);
+
+$("#color_me_id").click(function(){
+  $('#name_email_id').toggleClass("color_me");
+})
