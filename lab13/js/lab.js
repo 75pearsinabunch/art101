294026179e765ee04a223ece2e9388e650e8de7d
75pearsinabunch <75pearsinabunch@gmail.com>
2021-02-25 13:45:53 -0800

    Update lab.js

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -36,45 +36,42 @@ str =+ "Fizz";
 $("#output").append("<p>" + str + "</p>");
 $("#output").append*/
 
-number = 4;
-outputEl = document.getElementById("output");
-function getFactorObj() {
-    var empty = {};
-    for (var modNum=0; modNum<number; modNum++) {
-        numId = "num" + modNum;
-        textId = "text" + modNum;
-        numValue = document.getElementById(numId).value;
-        textValue = document.getElementById(textId).value;
-        console.log(modNum + ") num:", numValue, "text:", textValue)
-        if (numValue && textValue) {
-            empty[numValue] = textValue;
-        }
-    }
-    return empty;
-}
-function outputToPage(str) {
-    newEl = document.createElement("p");
-    newEl.innerHTML = str;
-    outputEl.appendChild(newEl);
-}
-function fizzBuzzBoom(maxNums, empty) {
-    for (var num=0; num<maxNums; num++) {
-        var outputStr = "";
-        for (var modNum in empty) {
-            if (num % modNum == 0) {
-                outputStr += empty[modNum];
-            }
-        }
-        if (outputStr) {
-            outputStr = " - " + outputStr + "!";
-        }
-        outputToPage(num.toString() + outputStr)
-    }
-}
-document.getElementById("submit").addEventListener("click", function() {
-    var max = document.getElementById("max").value;
-    var empty = getFactorObj();
-    outputEl.innerHTML = "";
-    fizzBuzzBoom(max, empty);
-    outputEl.classList.add("cols");
-})
+var superString= "";
+for (var numb= 1; numb <= 200; numb++) {
+  if (numb % 105 == 0) {
+    console.log ("fizzbuzzboom");
+    superString += numb + " - FizzBuzzBoom!<br>";
+  }
+  else if (numb % 15 == 0) {
+    console.log ("fizzbuzz");
+    superString += numb + " - FizzBuzz!<br>";
+  }
+  else if (numb % 35 == 0) {
+    console.log ("buzzboom");
+    superString += numb + " - BuzzBoom!<br>";
+  }
+  else if (numb % 21 == 0) {
+    console.log ("fizzboom");
+    superString += numb + " - FizzBoom!<br>";
+  }
+  //If the number is a multiple of 3, if should print "Fizz!"
+  else if (numb % 3 == 0) {
+    console.log ("fizz");
+    superString += numb + " - Fizz!<br>";
+  }
+  //If the number is a multiple of 5, it should print "Buzz!"
+  else if (numb % 5 == 0) {
+    console.log ("buzz");
+    superString += numb + " - Buzz!<br>";
+  }
+  //If the number is a multiple of 7, it should print "Boom!"
+  else if (numb % 7 == 0) {
+    console.log ("boom");
+    superString += numb + " - Boom!<br>";
+  }
+  else {console.log(numb)
+    superString += numb + "<br>";
+  }
+};
+
+$("#output").html(superString);
