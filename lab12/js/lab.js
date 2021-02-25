bd6007398869091c0b464c7ff87bba9d7b7de8be
75pearsinabunch <75pearsinabunch@gmail.com>
2021-02-25 13:46:25 -0800

    Update lab.js

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -21,7 +21,6 @@ function sortingHat(str) {
     return "Slytherin"
   }
 }
-
 var myButton = document. getElementById("button");
 myButton.addEventListner("click", function(){
   var name = document.getElementById("input").value;
@@ -29,30 +28,20 @@ myButton.addEventListner("click", function(){
   newText = "<p> The Sorting Hat has sorted you into "+house+"</p>";
   document.getElementById("output").innerHTML = newText;
 })
-
-
 */
-function getHouse(num){
-  var remainder = nume % 4;
-  if (remainder == 0){
-    var str = "Gryffindor";
-  }else if (remainder == 1){
-    var str = "Ravenclaw";
-  }else if (remainder == 2){
-    var str = "Hufflepuff";
-  }else if (remainder == 3){
-    var str = "Slytherin";
-  }
-  return str;
-}
-
-$("#button").click(function(){
+$("#button").click(function() {runSort()});
+function runSort() {
   var name = $("#input").val();
-  console.log(name);
-  var nameLength = name.length;
-  console.log(namelength);
-  var house = getHouse(nameLength);
-  console.log(house);
-  $("#output").html("<h1>" + house + "<h1>"")
-
-})
+  var yourHouse = sortingHat(name);
+  var houseName = ["Gryffindor","Ravenclaw","Slytherin","Hufflepuff"];
+  $("#output").html("The sorting hat has sorted you into " + houseName[yourHouse]);
+}
+function sortingHat(string) {
+  var length = string.length;
+  if (length == 0) {
+    return 0;
+  } else {
+    var mod = ((length % 4)+1);
+    return mod;
+  }
+}
