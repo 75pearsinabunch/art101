d08c7d3c6626ad7f89f3bb0769278cfc39cb74c3
75pearsinabunch <75pearsinabunch@gmail.com>
2021-02-28 11:20:39 -0800

    Update lab.js

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -36,42 +36,41 @@ str =+ "Fizz";
 $("#output").append("<p>" + str + "</p>");
 $("#output").append*/
 
-var superString= "";
-for (var numb= 1; numb <= 200; numb++) {
-  if (numb % 105 == 0) {
+var oneLongString= "";
+for (var num= 1; num <= 200; num++) {
+  if (num % 105 == 0) {
     console.log ("fizzbuzzboom");
-    superString += numb + " - FizzBuzzBoom!<br>";
+    oneLongString += num + " - FizzBuzzBoom!<br>";
   }
-  else if (numb % 15 == 0) {
+  else if (num % 15 == 0) {
     console.log ("fizzbuzz");
-    superString += numb + " - FizzBuzz!<br>";
+    oneLongString += num + " - FizzBuzz!<br>";
   }
-  else if (numb % 35 == 0) {
+  else if (num % 35 == 0) {
     console.log ("buzzboom");
-    superString += numb + " - BuzzBoom!<br>";
+    oneLongString += num + " - BuzzBoom!<br>";
   }
-  else if (numb % 21 == 0) {
+  else if (num % 21 == 0) {
     console.log ("fizzboom");
-    superString += numb + " - FizzBoom!<br>";
+    oneLongString += num + " - FizzBoom!<br>";
   }
-  //If the number is a multiple of 3, if should print "Fizz!"
-  else if (numb % 3 == 0) {
+  else if (num % 3 == 0) {
+    //you can comment out debugger to see it work properly
+    debugger;
     console.log ("fizz");
-    superString += numb + " - Fizz!<br>";
+    oneLongString += num + " - Fizz!<br>";
   }
-  //If the number is a multiple of 5, it should print "Buzz!"
-  else if (numb % 5 == 0) {
+  else if (num % 5 == 0) {
     console.log ("buzz");
-    superString += numb + " - Buzz!<br>";
+    oneLongString += num + " - Buzz!<br>";
   }
-  //If the number is a multiple of 7, it should print "Boom!"
-  else if (numb % 7 == 0) {
+  else if (num % 7 == 0) {
     console.log ("boom");
-    superString += numb + " - Boom!<br>";
+    oneLongString += num + " - Boom!<br>";
   }
-  else {console.log(numb)
-    superString += numb + "<br>";
+  else {console.log(num)
+    oneLongString += num + "<br>";
   }
 };
 
-$("#output").html(superString);
+$("#output").html(oneLongString);
