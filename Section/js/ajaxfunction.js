d6760d02b840e6b58b31a19741a85773770e9753
75pearsinabunch <75pearsinabunch@gmail.com>
2021-03-04 16:20:32 -0800

    Update ajaxfunction.js

--- Section/js/ajaxfunction.js
+++ Section/js/ajaxfunction.js
@@ -1,8 +1,29 @@
+// function getAjax(){
+// $.ajax({
+//   url:"Section/data/data.txt",
+//   type:"GET",
+//   dataType:"text",
+//   success: successFun,
+//   error:errorFun,
+//   complete:function(xhr, status){
+//     console.log("request is complete");
+//   }
+// });
+// }
+// function successFun(result){
+//   console.log(result);
+//   $("#ajax_text").html(result);
+// }
+// function errorFun(xhr, status, strErr){
+//   console.log("error");
+// }
+// $("#click_ajax").click(getAjax);
+
 function getAjax(){
 $.ajax({
-  url:"Section/data/data.txt",
+  url:"https://en.wikipedia.org/api/rest_v1/page/title/Warren_G._Harding",
   type:"GET",
-  dataType:"text",
+  dataType:"json",
   success: successFun,
   error:errorFun,
   complete:function(xhr, status){
@@ -12,9 +33,23 @@ $.ajax({
 }
 function successFun(result){
   console.log(result);
-  $("#ajax_text").html(result);
+  $("#ajax_text").html(result.items[0].title);
 }
 function errorFun(xhr, status, strErr){
-  console.lod("error");
+  console.log("error");
 }
 $("#click_ajax").click(getAjax);
+
+// $('#click_ajax').click(function(){
+//   var apiURL = "https://en.wikipedia.org/api/rest_v1/page/summary/Main_Page?redirect=true";
+//   console.log(apiURL);
+//   $.ajax({
+//     url: apiURL,
+//     data: { },
+//     type: "GET",
+//     dataTYPE: "json",
+//   })
+//   .done(function(data){
+//     $('#ajax_text').html(data.text);
+//   })
+// })
